6bb2d6ea1ad1b4cb8f9b4021b04109a221e8c007
Anna Dontcova <anna.dontcova@doctolib.com>
2023-06-20 20:03:39 +0200

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React deployed on Netlify</h1>
+      <h1>Vite + React deployed on Netlify deployed again</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
